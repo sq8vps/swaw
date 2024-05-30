aab3eb7a877dfa9fd93f4555ab0936148818297a
BartlomiejWos <bartlomiej.wos01@gmail.com>
2024-05-30 17:12:59 +0200

    Max30102

---
+++ max30102v2/Core/Inc/MAX30102.h
@@ -0,0 +1,183 @@
+/** \file max30102.h ******************************************************
+
+*	Website: https://msalamon.pl/palec-mi-pulsuje-pulsometr-max30102-pod-kontrola-stm32/
+*	GitHub:  https://github.com/lamik/MAX30102_STM32_HAL
+*
+*/
+#ifndef MAX30102_H_
+#define MAX30102_H_
+
+#define MAX30102_MEASUREMENT_SECONDS 	  	5	/// 
+#define MAX30102_SAMPLES_PER_SECOND		  	50	/// 50, 100, 200, 400, 800, 100, 1600, 3200 sample rating
+#define MAX30102_FIFO_ALMOST_FULL_SAMPLES 	17	///
+
+#define MAX30102_BUFFER_LENGTH	((MAX30102_MEASUREMENT_SECONDS + 1) * MAX30102_SAMPLES_PER_SECOND)	/// 
+
+/*--------------------REGISTER MAPS AND DESCRIPTIONS--------------------*/  
+
+/* STATUS */
+#define INTERRUPT_STATUS_1          0x00    /// Read
+#define INTERRUPT_STATUS_2          0x01    /// Read
+#define INTERRUPT_ENABLE_1          0x02    /// Read
+#define INTERRUPT_ENABLE_2          0x03    /// Read/Write 
+
+/* FIFO */
+#define FIFO_WRITE_POINTER          0x04    /// Points to the location where the MAX30102 writes the next sample (advances for each sample pushed on to the FIFO), can be changed when MODE[2:0]. 
+#define OVERFLOW_COUNTER            0x05    /// Read/Write 
+#define FIFO_READ_POINTER           0x06    /// Read/Write 
+#define FIFO_DATA_REGISTER          0x07    /// Read/Write 
+
+/* CONFIGURATION */
+#define FIFO_CONFIGURATION          0x08    /// Read/Write 
+#define MODE_CONFIGURATION          0x09    /// Read/Write 
+#define SPO2_CONFIGURATION          0x0A    /// Read/Write 
+
+#define LED_1_PA                    0x0C    /// LED 1 Pulse Amplitude Read/Write Read/Writ
+#define LED_2_PA                    0x0D    /// LED 2 Pulse Amplitude Read/Write 
+
+#define M_LED_CTRL_REG_1            0x11    /// Multi-LED Mode Control Register 1     Read/Write 
+#define M_LED_CTRL_REG_2            0x12    /// Multi-LED Mode Control Register 2     Read/Write 
+
+/* DIE TEMPERATURE */
+#define DIE_TEMP_INT                0x1F    /// Die Temperature Integer Read
+#define DIE_TEMP_FRAC               0x20    /// Die Temperature Fraction Read
+#define DIE_TEMP_CONFIG             0x21    /// Die Temperature Config Read/Write 
+
+/* PART ID */
+#define REVISION_ID                 0xFE    /// 
+#define PART_ID                     0xFF    /// 
+
+/* SLAVE ID */
+#define MAX30102_ADDRESS 			0xAE	/// (0x57<<1)
+
+/*--------------------CALIBRATION--------------------*/
+#define MAX30102_IR_LED_CURRENT_LOW				0x01	///
+#define MAX30102_RED_LED_CURRENT_LOW			0x00	///
+#define MAX30102_IR_LED_CURRENT_HIGH			0x24	///
+#define MAX30102_RED_LED_CURRENT_HIGH			0x24	///
+#define MAX30102_IR_VALUE_FINGER_ON_SENSOR 		1600	///
+#define MAX30102_IR_VALUE_FINGER_OUT_SENSOR 	50000	/// 
+
+
+#define	INT_A_FULL_BIT					7	///	
+#define	INT_PPG_RDY_BIT					6	///
+#define	INT_ALC_OVF_BIT					5	///
+#define	INT_DIE_TEMP_RDY_BIT			1	///
+#define	INT_PWR_RDY_BIT					0	/// Only STATUS register
+
+/*--------------------FIFO CONFIGURATION--------------------*/
+#define FIFO_CONF_SMP_AVE_BIT 			7
+#define FIFO_CONF_SMP_AVE_LENGHT 		3
+#define FIFO_CONF_FIFO_ROLLOVER_EN_BIT 	4
+#define FIFO_CONF_FIFO_A_FULL_BIT 		3
+#define FIFO_CONF_FIFO_A_FULL_LENGHT 	4
+
+#define FIFO_SMP_AVE_1					0	/// No. Of Samples Averaged Per Fifo Sample = 1 (No Averaging)	
+#define FIFO_SMP_AVE_2					1	/// No. Of Samples Averaged Per Fifo Sample = 2 
+#define FIFO_SMP_AVE_4					2 	/// No. Of Samples Averaged Per Fifo Sample = 4 
+#define FIFO_SMP_AVE_8					3 	/// No. Of Samples Averaged Per Fifo Sample = 8 
+#define FIFO_SMP_AVE_16					4 	/// No. Of Samples Averaged Per Fifo Sample = 16 
+#define FIFO_SMP_AVE_32					5 	/// No. Of Samples Averaged Per Fifo Sample = 32
+
+/*--------------------MODE CONFIGURATION--------------------*/
+#define MODE_HEART_RATE_MODE	2
+#define MODE_SPO2_MODE			3
+#define MODE_MULTI_LED_MODE		7
+
+/*--------------------SPO2 CONFIGURATION-------------------- */
+#define SPO2_CONF_ADC_RGE_BIT		6
+#define SPO2_CONF_ADC_RGE_LENGTH	2
+#define SPO2_CONF_SR_BIT			4
+#define SPO2_CONF_SR_LENGTH			3
+#define SPO2_CONF_LED_PW_BIT		1
+#define SPO2_CONF_LED_PW_LENGTH		2
+
+#define	SPO2_ADC_RGE_2048	0
+#define	SPO2_ADC_RGE_4096	1
+#define	SPO2_ADC_RGE_8192	2
+#define	SPO2_ADC_RGE_16384	3
+
+#define	SPO2_SAMPLE_RATE_50		0
+#define	SPO2_SAMPLE_RATE_100	1
+#define	SPO2_SAMPLE_RATE_200	2
+#define	SPO2_SAMPLE_RATE_400	3
+#define	SPO2_SAMPLE_RATE_800	4
+#define	SPO2_SAMPLE_RATE_1000	5
+#define	SPO2_SAMPLE_RATE_1600	6
+#define	SPO2_SAMPLE_RATE_3200	7	
+
+#if(MAX30102_SAMPLES_PER_SECOND == 50)
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_50
+#elif((MAX30102_SAMPLES_PER_SECOND == 100))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_100
+#elif((MAX30102_SAMPLES_PER_SECOND == 200))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_200
+#elif((MAX30102_SAMPLES_PER_SECOND == 400))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_400
+#elif((MAX30102_SAMPLES_PER_SECOND == 800))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_800
+#elif((MAX30102_SAMPLES_PER_SECOND == 1000))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_1000
+#elif((MAX30102_SAMPLES_PER_SECOND == 1600))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_1600
+#elif((MAX30102_SAMPLES_PER_SECOND == 3200))
+#define SPO2_SAMPLE_RATE SPO2_SAMPLE_RATE_3200
+#else
+#error "Wrong Sample Rate value. Use 50, 100, 200, 400, 800, 1000, 1600 or 3200."
+#endif
+
+#define	SPO2_PULSE_WIDTH_69			0
+#define	SPO2_PULSE_WIDTH_118		1
+#define	SPO2_PULSE_WIDTH_215		2
+#define	SPO2_PULSE_WIDTH_411		3
+
+
+typedef enum{
+	MAX30102_ERROR 	= 0,
+	MAX30102_OK 	= 1
+} MAX30102_STATUS;
+
+
+
+/*--------------------FUNCTION DEFINITIONS--------------------*/
+
+void Max30102_Init(I2C_HandleTypeDef *i2c);
+MAX30102_STATUS Max30102_ReadFifo(volatile uint32_t *pun_red_led, volatile uint32_t *pun_ir_led);
+void Max30102_Write_Reg(uint8_t uch_addr, uint8_t uch_data);
+void Max30102_Read_Reg(uint8_t uch_addr, uint8_t *puch_data);
+
+/* INTERRUPTS */
+MAX30102_STATUS Max30102_ReadInterruptStatus(uint8_t *Status);
+void Max30102_SetIntAlmostFullEnabled(uint8_t Enable);
+void Max30102_SetIntFifoDataReadyEnabled(uint8_t Enable);
+#ifdef MAX30102_USE_INTERNAL_TEMPERATURE
+MAX30102_STATUS Max30102_SetIntInternalTemperatureReadyEnabled(uint8_t Enable);
+#endif
+void Max30102_InterruptCallback(void);
+
+/* FIFO CONFIGURATIONS */
+void Max30102_FifoWritePointer(uint8_t Address);
+void Max30102_FifoOverflowCounter(uint8_t Address);
+void Max30102_FifoReadPointer(uint8_t Address);
+void Max30102_FifoSampleAveraging(uint8_t Value);
+void Max30102_FifoRolloverEnable(uint8_t Enable);
+void Max30102_FifoAlmostFullValue(uint8_t Value); // 17-32 samples ready in FIFO
+
+/* MODE CONFIGURATION */
+void Max30102_SetMode(uint8_t Mode);
+
+/* SPO2 CONFIGURATION */
+void Max30102_SpO2AdcRange(uint8_t Value);
+void Max30102_SpO2SampleRate(uint8_t Value);
+void Max30102_SpO2LedPulseWidth(uint8_t Value);
+
+/* LEDs Pulse Amplitute Configuration */
+void Max30102_Led1PulseAmplitude(uint8_t Value);
+void Max30102_Led2PulseAmplitude(uint8_t Value);
+
+/* USAGE FUNCTIONS */
+void Max30102_Task(void);
+int32_t Max30102_GetHeartRate(void);
+int32_t Max30102_GetSpO2Value(void);
+
+#endif /* MAX30102_H_ */
---
+++ max30102v2/Core/Src/MAX30102.c
@@ -0,0 +1,397 @@
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+#include "MAX30102.h"
+#include "algorithm.h"
+#include "stdint.h"
+#define I2C_TIMEOUT	1
+
+I2C_HandleTypeDef *i2c_max30102;
+
+volatile uint32_t IrBuffer[MAX30102_BUFFER_LENGTH]; /// IR LED sensor data
+volatile uint32_t RedBuffer[MAX30102_BUFFER_LENGTH]; /// Red LED sensor data
+volatile uint32_t BufferHead;
+volatile uint32_t BufferTail;
+volatile uint32_t CollectedSamples;
+volatile uint8_t IsFingerOnScreen;
+int32_t Sp02Value;
+int8_t Sp02IsValid;
+int32_t HeartRate;
+int8_t IsHrValid;
+
+typedef enum
+{
+	MAX30102_STATE_BEGIN,
+	MAX30102_STATE_CALIBRATE,
+	MAX30102_STATE_CALCULATE_HR,
+	MAX30102_STATE_COLLECT_NEXT_PORTION
+}MAX30102_STATE;
+	MAX30102_STATE StateMachine;
+
+
+void Max30102_Write_Reg(uint8_t uch_addr, uint8_t uch_data){
+
+	HAL_I2C_Mem_Write(i2c_max30102, MAX30102_ADDRESS, uch_addr, 1, &uch_data, 1, I2C_TIMEOUT);
+}
+
+void Max30102_Read_Reg(uint8_t uch_addr, uint8_t *puch_data){
+
+	HAL_I2C_Mem_Read(i2c_max30102, MAX30102_ADDRESS, uch_addr, 1, puch_data, 1, I2C_TIMEOUT);
+}
+
+void Max30102_WriteRegisterBit(uint8_t Register, uint8_t Bit, uint8_t Value){
+
+	uint8_t tmp;
+    Max30102_Read_Reg(Register, &tmp);
+		
+	tmp &= ~(1<<Bit);
+	tmp |= (Value&0x01)<<Bit;
+	Max30102_Write_Reg(Register, tmp);
+}
+
+void Max30102_FifoWritePointer(uint8_t Address){
+
+	Max30102_Write_Reg(FIFO_WRITE_POINTER,(Address & 0x1F));  //FIFO_WR_PTR[4:0]
+}
+
+void Max30102_FifoOverflowCounter(uint8_t Address){
+
+	Max30102_Write_Reg(OVERFLOW_COUNTER,(Address & 0x1F));  //OVF_COUNTER[4:0]
+}
+
+void Max30102_FifoReadPointer(uint8_t Address){
+
+	Max30102_Write_Reg(FIFO_READ_POINTER,(Address & 0x1F));  //FIFO_RD_PTR[4:0]
+}
+
+void Max30102_FifoSampleAveraging(uint8_t Value) {
+
+	uint8_t tmp;
+	Max30102_Read_Reg(FIFO_CONFIGURATION, &tmp);
+		
+	tmp &= ~(0x07);
+	tmp |= (Value&0x07)<<5;
+	Max30102_Write_Reg(FIFO_CONFIGURATION, tmp);
+}
+
+void Max30102_FifoRolloverEnable(uint8_t Enable){
+
+	Max30102_WriteRegisterBit(FIFO_CONFIGURATION, FIFO_CONF_FIFO_ROLLOVER_EN_BIT, (Enable & 0x01));
+}
+
+void  Max30102_FifoAlmostFullValue(uint8_t Value){
+
+	if(Value < 17) Value = 17;
+	if(Value > 32) Value = 32;
+	Value = 32 - Value;
+	uint8_t tmp;
+	Max30102_Read_Reg(FIFO_CONFIGURATION, &tmp);
+		
+	tmp &= ~(0x0F);
+	tmp |= (Value & 0x0F);
+	Max30102_Write_Reg(FIFO_CONFIGURATION, tmp);
+}
+
+void Max30102_SetMode(uint8_t Mode){
+
+	uint8_t tmp;
+	Max30102_Read_Reg(MODE_CONFIGURATION, &tmp);
+		
+	tmp &= ~(0x07);
+	tmp |= (Mode & 0x07);
+	Max30102_Write_Reg(MODE_CONFIGURATION, tmp);
+}
+
+void Max30102_SpO2AdcRange(uint8_t Value){
+
+	uint8_t tmp;
+	Max30102_Read_Reg(SPO2_CONFIGURATION, &tmp);
+
+	tmp &= ~(0x03);
+	tmp |= ((Value & 0x03) << 5);
+    Max30102_Write_Reg(SPO2_CONFIGURATION, tmp);
+}
+
+void Max30102_SpO2SampleRate(uint8_t Value){
+
+	uint8_t tmp;
+	Max30102_Read_Reg(SPO2_CONFIGURATION, &tmp);
+
+	tmp &= ~(0x07);
+	tmp |= ((Value & 0x07) << 2);
+	Max30102_Write_Reg(SPO2_CONFIGURATION, tmp);
+}
+
+void Max30102_SpO2LedPulseWidth(uint8_t Value){
+
+	uint8_t tmp;
+	Max30102_Read_Reg(SPO2_CONFIGURATION, &tmp);
+	
+	tmp &= ~(0x03);
+	tmp |= (Value & 0x03);
+	Max30102_Write_Reg(SPO2_CONFIGURATION, tmp);
+		
+}
+
+void Max30102_SetIntAlmostFullEnabled(uint8_t Enable){
+
+	return Max30102_WriteRegisterBit(INTERRUPT_ENABLE_1, INT_A_FULL_BIT, Enable);
+}
+
+void Max30102_SetIntFifoDataReadyEnabled(uint8_t Enable){
+
+	return Max30102_WriteRegisterBit(INTERRUPT_ENABLE_1, INT_PPG_RDY_BIT, Enable);
+}
+
+MAX30102_STATUS Max30102_ReadFifo(volatile uint32_t *pun_red_led, volatile uint32_t *pun_ir_led)
+{
+	uint32_t un_temp;
+	*pun_red_led=0;
+	*pun_ir_led=0;
+	uint8_t ach_i2c_data[6];
+
+	if(HAL_I2C_Mem_Read(i2c_max30102, MAX30102_ADDRESS, FIFO_DATA_REGISTER, 1, ach_i2c_data, 6, I2C_TIMEOUT) != HAL_OK)
+	{
+		return MAX30102_ERROR;
+	}
+	un_temp=(unsigned char) ach_i2c_data[0];
+	un_temp<<=16;
+	*pun_red_led+=un_temp;
+	un_temp=(unsigned char) ach_i2c_data[1];
+	un_temp<<=8;
+	*pun_red_led+=un_temp;
+	un_temp=(unsigned char) ach_i2c_data[2];
+	*pun_red_led+=un_temp;
+
+	un_temp=(unsigned char) ach_i2c_data[3];
+	un_temp<<=16;
+	*pun_ir_led+=un_temp;
+	un_temp=(unsigned char) ach_i2c_data[4];
+	un_temp<<=8;
+	*pun_ir_led+=un_temp;
+	un_temp=(unsigned char) ach_i2c_data[5];
+	*pun_ir_led+=un_temp;
+	*pun_red_led&=0x03FFFF;  //Mask MSB [23:18]
+	*pun_ir_led&=0x03FFFF;  //Mask MSB [23:18]
+
+	return MAX30102_OK;
+}
+
+// #ifdef MAX30102_USE_INTERNAL_TEMPERATURE
+// MAX30102_STATUS Max30102_SetIntInternalTemperatureReadyEnabled(uint8_t Enable)
+// {
+
+// 	return Max30102_WriteRegisterBit(INTERRUPT_ENABLE_2, INT_DIE_TEMP_RDY_BIT, Enable);
+// }
+// #endif
+
+MAX30102_STATUS Max30102_ReadInterruptStatus(uint8_t *Status)
+{
+	uint8_t tmp;
+	*Status = 0;
+
+	Max30102_Read_Reg(INTERRUPT_STATUS_1, &tmp);
+	
+	*Status |= tmp & 0xE1; // 3 highest bits
+// #ifdef MAX30102_USE_INTERNAL_TEMPERATURE
+// 	if(MAX30102_OK != Max30102_ReadReg(INTERRUPT_STATUS_2, &tmp))
+// 		return MAX30102_ERROR;
+// 	*Status |= tmp & 0x02;
+// #endif
+	return MAX30102_OK;
+}
+
+
+void Max30102_InterruptCallback(void)
+{
+	uint8_t Status;
+	while(MAX30102_OK != Max30102_ReadInterruptStatus(&Status));
+
+	// Almost Full FIFO Interrupt handle
+	if(Status & (1<<INT_A_FULL_BIT))
+	{
+		for(uint8_t i = 0; i < MAX30102_FIFO_ALMOST_FULL_SAMPLES; i++)
+		{
+			while(MAX30102_OK != Max30102_ReadFifo((RedBuffer+BufferHead), (IrBuffer+BufferHead)));
+			if(IsFingerOnScreen)
+			{
+				if(IrBuffer[BufferHead] < MAX30102_IR_VALUE_FINGER_OUT_SENSOR) IsFingerOnScreen = 0;
+			}
+			else
+			{
+				if(IrBuffer[BufferHead] > MAX30102_IR_VALUE_FINGER_ON_SENSOR) IsFingerOnScreen = 1;
+			}
+			BufferHead = (BufferHead + 1) % MAX30102_BUFFER_LENGTH;
+			CollectedSamples++;
+		}
+	}
+
+	// New FIFO Data Ready Interrupt handle
+	if(Status & (1<<INT_PPG_RDY_BIT))
+	{
+		while(MAX30102_OK != Max30102_ReadFifo((RedBuffer+BufferHead), (IrBuffer+BufferHead)));
+		if(IsFingerOnScreen)
+		{
+			if(IrBuffer[BufferHead] < MAX30102_IR_VALUE_FINGER_OUT_SENSOR) IsFingerOnScreen = 0;
+		}
+		else
+		{
+			if(IrBuffer[BufferHead] > MAX30102_IR_VALUE_FINGER_ON_SENSOR) IsFingerOnScreen = 1;
+		}
+		BufferHead = (BufferHead + 1) % MAX30102_BUFFER_LENGTH;
+		CollectedSamples++;
+	}
+
+	//  Ambient Light Cancellation Overflow Interrupt handle
+	if(Status & (1<<INT_ALC_OVF_BIT))
+	{
+
+	}
+
+	// Power Ready Interrupt handle
+	if(Status & (1<<INT_PWR_RDY_BIT))
+	{
+	}
+// #ifdef MAX30102_USE_INTERNAL_TEMPERATURE
+// 	// Internal Temperature Ready Interrupt handle
+// 	if(Status & (1<<INT_DIE_TEMP_RDY_BIT))
+// 	{
+
+// 	}
+// #endif
+}
+
+
+/* LEDs Pulse Amplitute Configuration	LED Current = Value * 0.2 mA */	
+
+void Max30102_Led1PulseAmplitude(uint8_t Value){
+
+	Max30102_Write_Reg(LED_1_PA, Value);
+	
+}
+
+void Max30102_Led2PulseAmplitude(uint8_t Value){
+
+	Max30102_Write_Reg(LED_2_PA, Value);
+
+}
+
+
+/* Usage Functions */
+
+int32_t Max30102_GetHeartRate(void)
+{
+	return HeartRate;
+}
+
+
+int32_t Max30102_GetSpO2Value(void)
+{
+	return Sp02Value;
+}
+
+
+
+
+void Max30102_Task(void)
+{
+	switch(StateMachine)
+	{
+		case MAX30102_STATE_BEGIN:
+			HeartRate = 0;
+			Sp02Value = 0;
+			if(IsFingerOnScreen)
+			{
+				CollectedSamples = 0;
+				BufferTail = BufferHead;
+				Max30102_Led1PulseAmplitude(MAX30102_RED_LED_CURRENT_HIGH);
+				Max30102_Led2PulseAmplitude(MAX30102_IR_LED_CURRENT_HIGH);
+				StateMachine = MAX30102_STATE_CALIBRATE;
+			}
+			break;
+
+		case MAX30102_STATE_CALIBRATE:
+				if(IsFingerOnScreen)
+				{
+					if(CollectedSamples > (MAX30102_BUFFER_LENGTH-MAX30102_SAMPLES_PER_SECOND))
+					{
+						StateMachine = MAX30102_STATE_CALCULATE_HR;
+					}
+				}
+				else
+				{
+					Max30102_Led1PulseAmplitude(MAX30102_RED_LED_CURRENT_LOW);
+					Max30102_Led2PulseAmplitude(MAX30102_IR_LED_CURRENT_LOW);
+					StateMachine = MAX30102_STATE_BEGIN;
+				}
+			break;
+
+		case MAX30102_STATE_CALCULATE_HR:
+			if(IsFingerOnScreen)
+			{
+				maxim_heart_rate_and_oxygen_saturation(IrBuffer, RedBuffer, MAX30102_BUFFER_LENGTH-MAX30102_SAMPLES_PER_SECOND, BufferTail, &Sp02Value, &Sp02IsValid, &HeartRate, &IsHrValid);
+				BufferTail = (BufferTail + MAX30102_SAMPLES_PER_SECOND) % MAX30102_BUFFER_LENGTH;
+				CollectedSamples = 0;
+				StateMachine = MAX30102_STATE_COLLECT_NEXT_PORTION;
+			}
+			else
+			{
+				Max30102_Led1PulseAmplitude(MAX30102_RED_LED_CURRENT_LOW);
+				Max30102_Led2PulseAmplitude(MAX30102_IR_LED_CURRENT_LOW);
+				StateMachine = MAX30102_STATE_BEGIN;
+			}
+			break;
+
+		case MAX30102_STATE_COLLECT_NEXT_PORTION:
+			if(IsFingerOnScreen)
+			{
+				if(CollectedSamples > MAX30102_SAMPLES_PER_SECOND)
+				{
+					StateMachine = MAX30102_STATE_CALCULATE_HR;
+				}
+			}
+			else
+			{
+				Max30102_Led1PulseAmplitude(MAX30102_RED_LED_CURRENT_LOW);
+				Max30102_Led2PulseAmplitude(MAX30102_IR_LED_CURRENT_LOW);
+				StateMachine = MAX30102_STATE_BEGIN;
+			}
+			break;
+	}
+}
+
+void Max30102_Init(I2C_HandleTypeDef *i2c){
+	uint8_t uch_dummy;
+	i2c_max30102 = i2c;
+ 	Max30102_Write_Reg(MODE_CONFIGURATION,0x40);	/// Reset Register's 		
+	Max30102_Read_Reg(0,&uch_dummy);
+	Max30102_FifoWritePointer(0x00);				
+
+	Max30102_FifoOverflowCounter(0x00);				
+	
+	Max30102_FifoReadPointer(0x00);					
+	
+	Max30102_FifoSampleAveraging(FIFO_SMP_AVE_1);	
+		
+    Max30102_FifoRolloverEnable(0);
+
+	Max30102_FifoAlmostFullValue(MAX30102_FIFO_ALMOST_FULL_SAMPLES);
+	
+	Max30102_SetMode(MODE_SPO2_MODE);
+	
+	Max30102_SpO2AdcRange(SPO2_ADC_RGE_4096);
+
+	Max30102_SpO2SampleRate(SPO2_SAMPLE_RATE);
+
+	Max30102_SpO2LedPulseWidth(SPO2_PULSE_WIDTH_411);
+
+    Max30102_Led1PulseAmplitude(MAX30102_RED_LED_CURRENT_LOW);
+
+	Max30102_Led2PulseAmplitude(MAX30102_IR_LED_CURRENT_LOW);
+	
+	Max30102_SetIntAlmostFullEnabled(1);
+	
+	Max30102_SetIntFifoDataReadyEnabled(1);
+
+	StateMachine = MAX30102_STATE_BEGIN;
+}
